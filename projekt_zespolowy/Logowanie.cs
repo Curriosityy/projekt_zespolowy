ee336aa2584d6b93dc3370fdf3bf464ee349a564
Xhauz <karmazynowymagazynowy@gmail.com>
2018-05-21 15:17:27 +0200

    Projekt 0.1
    Połączenie z bazą danych

--- projekt_zespolowy/Logowanie.cs
+++ projekt_zespolowy/Logowanie.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace projekt_zespolowy
 {
@@ -16,6 +18,23 @@ namespace projekt_zespolowy
         {
             InitializeComponent();
         }
-          
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection cnn;
+            string connetionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=projekt;";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+
+        }
     }
 }
