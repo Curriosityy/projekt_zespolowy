a64310aa70c82018948f04995a2f00d480a2c732
Xhauz <karmazynowymagazynowy@gmail.com>
2018-05-22 01:44:21 +0200

    Projekt 0.1.2
    Logowanie

--- projekt_zespolowy/Logowanie.cs
+++ projekt_zespolowy/Logowanie.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-
 
 namespace projekt_zespolowy
 {
@@ -21,20 +14,69 @@ namespace projekt_zespolowy
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string uprawnienia;
+
             MySqlConnection cnn;
             string connetionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=projekt;";
             cnn = new MySqlConnection(connetionString);
-            try
+            cnn.Open();
+
+            /*   try
+               {
+                  cnn.Open();
+                  MessageBox.Show("Connection Open ! ");
+                  cnn.Close();
+               }
+               catch (Exception ex)
+               {
+                   MessageBox.Show("Can not open connection ! ");
+               }
+            */
+
+            if (string.IsNullOrEmpty(textBox1.Text))
             {
-                cnn.Open();
-                MessageBox.Show("Connection Open ! ");
-                cnn.Close();
+                MessageBox.Show("Proszę wprowadzić login");
+                this.textBox1.Focus();
+                return;
             }
-            catch (Exception ex)
+            if (string.IsNullOrEmpty(textBox2.Text))
             {
-                MessageBox.Show("Can not open connection ! ");
+                MessageBox.Show("Proszę wprowadzić hasło");
+                this.textBox1.Focus();
+                return;
             }
 
+            MySqlDataAdapter log = new MySqlDataAdapter("SELECT Count(*) FROM pracownik WHERE login='" + textBox1.Text + "' AND haslo='" + textBox2.Text + "'", cnn);
+            DataTable dt = new DataTable();
+            log.Fill(dt);
+
+            string SqlQuery = "SELECT uprawnienia FROM pracownik WHERE login='" + textBox1.Text + "'";
+            MySqlCommand comm = new MySqlCommand(SqlQuery, cnn);
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                //                using (MySqlDataReader oReader = comm.ExecuteReader())
+                //                    try
+                //                    {
+                //                        while (oReader.Read())
+                //                        {
+                //
+                //                        }
+
+                //                    }
+                this.Hide();
+                Szef s = new Szef();
+                s.Show();
+            }
+            else
+            {
+                MessageBox.Show("Złe hasło lub login");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
